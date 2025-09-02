6fa584aa21c19aa026c638eccdd5c2b41254cbd5
01-akm <kraadi4941@gmail.com>
2025-09-02 07:06:03 +0530

    Create an Express application

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 // Import necessary libraries
 const express = require('express');
 const path = require('path');
+
+// Create an Express application
+const app = express();
+const PORT = process.env.PORT || 3000;
