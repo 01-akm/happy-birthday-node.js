b234d2f1dfb67790ece6a92208de9b40b25fdfb2
01-akm <kraadi4941@gmail.com>
2025-09-02 07:06:21 +0530

    Middleware to handle JSON and URL-encoded data

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const path = require('path');
 // Create an Express application
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Middleware to handle JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
