5573f4bd3f76790d7a01ca34ebfdf163d1ae0f43
01-akm <kraadi4941@gmail.com>
2025-09-02 07:07:00 +0530

    This is where the form from phase4.html will send the message

--- server.js
+++ server.js
@@ -12,3 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Serve all the HTML, CSS, JS, and music files from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// --- The API Endpoint ---
+// This is where the form from phase4.html will send the message
+app.post('/api/save-message', (req, res) => {
