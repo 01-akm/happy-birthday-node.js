4892aa9a2b44c2a1d80934d391f277289d39c30e
01-akm <kraadi4941@gmail.com>
2025-09-02 07:19:11 +0530

    initalize indexscriptjs

---
+++ public/indexscript.js
@@ -0,0 +1,9 @@
+
+        const mainContent = document.getElementById('main-content');
+        const effectContainer = document.getElementById('effect-container');
+        const nextPageBtn = document.getElementById('next-page-btn');
+        const countdownEl = document.getElementById('countdown');
+        const startScreen = document.getElementById('start-screen');
+        const startBtn = document.getElementById('start-btn');
+
+        function startExperience() {
